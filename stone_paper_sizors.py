f0f17bb323f8dbd7d7e46f551769f3fa28c189ac
YASH YADAV <143890234+yasx50@users.noreply.github.com>
2023-09-25 20:49:27 +0530

    Add files via upload

---
+++ stone_paper_sizors.py
@@ -0,0 +1,48 @@
+import random
+#  stone paper sizor game
+# author milkosy(YASH YADAV)
+def win(comp,you):
+    if you==comp:
+        return None
+    if comp=='s':
+        you='p'
+        return True
+    elif you=='S':
+        return False
+    if comp=='p':
+        you='S'
+        return True
+    elif you=='s':
+        return False
+    if comp=='S':
+        you='s'
+        return True
+    elif you=='p':
+        return False
+    
+print("computer turn is completed now yours\n choose")
+randomno=random.randint(1,3)
+if randomno==1:
+    comp='s'
+elif randomno==2:
+    comp='p'
+elif randomno==3:
+    comp='S'
+
+
+you=input("for stone(s),for paper(p),for sizors(S)\n")
+a=win(comp,you)
+
+
+print(f"computer choose  {comp} and you choose  {you}")
+
+
+if a==None:
+    print("this game is tie :| ")
+if a==False:
+    print("you win this game :)")
+if a==True:
+    print("you loose the game :< ")
+
+
+
